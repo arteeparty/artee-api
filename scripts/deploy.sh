4b9ac550bb3d5a6434faba362ff02eed48ecbc47
Artee Party <stew@airpost.net>
2017-09-12 21:13:00 +1000

    More info about deploy step

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,4 @@
-scp -i $TRAVIS_BUILD_DIR/github_deploy_key $TRAVIS_BUILD_DIR/build/artee-api-linux64 artee@api.artee.party:/home/artee
-ssh -i $TRAVIS_BUILD_DIR/github_deploy_key artee@api.artee.party sudo /bin/bash /home/artee/deploy.sh
+echo $TRAVIS_BUILD_DIR
+ls $TRAVIS_BUILD_DIR
+scp $TRAVIS_BUILD_DIR/build/artee-api-linux64 artee@api.artee.party:/home/artee
+ssh artee@api.artee.party sudo /bin/bash /home/artee/deploy.sh
